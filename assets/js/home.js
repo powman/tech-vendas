8faa62153cfdfe456eaa9eb1dc841cdbb9059169
powman <paulophpweb@gmail.com>
2022-06-05 22:28:31 -0300

    Update

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,3 +1,5 @@
+const myCarousel = Vue.ref(null);
+
 const App = {
 	components: {
 		Carousel: VueCarousel.Carousel,
@@ -9,16 +11,17 @@ const App = {
 		return {
             banners: [
                 {
+                    id: 1,
                     title: "iPhone 13 Grafite 256gb",
                     photo: "assets/images/iphone13.png",
                     discount: "17% OFF",
                     description: "Tela 6.1, câmera dupla de 12MP",
                     price_regular: "7.500,00",
-                    price_discount: "5.500,00",
                     url: "https://www.google.com.br",
                     target: "_blank",
                 },
                 {
+                    id: 2,
                     title: "iPhone 14 Grafite 256gb",
                     photo: "assets/images/iphone13.png",
                     discount: "1% OFF",
@@ -29,15 +32,193 @@ const App = {
                     target: "_blank",
                 },
             ],
-			// carousel settings
+            produtos_destaque: [
+                {
+                    id: 1,
+                    title: "Mini drone DJI Mavic Mini 2 DRDJI017",
+                    photo: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/HM9Q2?wid=572&hei=572&fmt=jpeg&qlt=95&.v=1534191258296",
+                    description: "Single com câmera 4K light gray DRDJI017 ",
+                    price_regular: "7.500,00",
+                    price_discount: "5.500,00",
+                    url: "https://www.google.com.br"
+                },
+                {
+                    id: 2,
+                    title: "Mini drone DJI Mavic Mini 2 DRDJI017",
+                    photo: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/HM9Q2?wid=572&hei=572&fmt=jpeg&qlt=95&.v=1534191258296",
+                    description: "Single com câmera 4K light gray DRDJI017 ",
+                    price_regular: "7.500,00",
+                    url: "https://www.google.com.br"
+                },
+                {
+                    id: 3,
+                    title: "Mini drone DJI Mavic Mini 2 DRDJI017",
+                    photo: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/HM9Q2?wid=572&hei=572&fmt=jpeg&qlt=95&.v=1534191258296",
+                    description: "Single com câmera 4K light gray DRDJI017 ",
+                    price_regular: "7.500,00",
+                    url: "https://www.google.com.br"
+                },
+                {
+                    id: 4,
+                    title: "Mini drone DJI Mavic Mini 2 DRDJI017",
+                    photo: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/HM9Q2?wid=572&hei=572&fmt=jpeg&qlt=95&.v=1534191258296",
+                    description: "Single com câmera 4K light gray DRDJI017 ",
+                    price_regular: "7.500,00",
+                    url: "https://www.google.com.br"
+                },
+                {
+                    id: 5,
+                    title: "Mini drone DJI Mavic Mini 2 DRDJI017",
+                    photo: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/HM9Q2?wid=572&hei=572&fmt=jpeg&qlt=95&.v=1534191258296",
+                    description: "Single com câmera 4K light gray DRDJI017 ",
+                    price_regular: "7.500,00",
+                    url: "https://www.google.com.br"
+                },
+            ],
+            filter_categorias: [
+                {
+                    id: 1,
+                    title: "Smartphones",
+                    url: "https://www.google.com.br"
+                },
+                {
+                    id: 2,
+                    title: "NOTEBOOKS",
+                    url: "https://www.google.com.br"
+                },
+                {
+                    id: 3,
+                    title: "DRONES",
+                    url: "https://www.google.com.br"
+                },
+                {
+                    id: 4,
+                    title: "SMARTWATCHS",
+                    url: "https://www.google.com.br"
+                },
+                {
+                    id: 5,
+                    title: "MONITORES",
+                    url: "https://www.google.com.br"
+                },
+            ],
+            filter_regiao: [
+                {
+                    id: 1,
+                    title: "Goiânia"
+                },
+                {
+                    id: 2,
+                    title: "Aparecida de Goiânia"
+                },
+                {
+                    id: 3,
+                    title: "Trindade"
+                },
+                {
+                    id: 4,
+                    title: "Senador Canedo"
+                }
+            ],
+            filter_price: [
+                {
+                    id: 1,
+                    title: "ATÉ R$800,00"
+                },
+                {
+                    id: 2,
+                    title: "R$801,00 - R$1.500,00"
+                },
+                {
+                    id: 3,
+                    title: "R$1.500,01 - R$2.500,00"
+                }
+            ],
+            filter_estado_produto: [
+                {
+                    id: 1,
+                    title: "Produto novo"
+                },
+                {
+                    id: 2,
+                    title: "Produto usado"
+                }
+            ],
+            produtos_home: [
+                {
+                    id: 1,
+                    title: "Mini drone DJI Mavic Mini 2 DRDJI017",
+                    photo: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/HM9Q2?wid=572&hei=572&fmt=jpeg&qlt=95&.v=1534191258296",
+                    description: "Single com câmera 4K light gray DRDJI017 ",
+                    price_regular: "7.500,00",
+                    price_discount: "5.500,00",
+                    url: "https://www.google.com.br"
+                },
+                {
+                    id: 2,
+                    title: "Mini drone DJI Mavic Mini 2 DRDJI017",
+                    photo: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/HM9Q2?wid=572&hei=572&fmt=jpeg&qlt=95&.v=1534191258296",
+                    description: "Single com câmera 4K light gray DRDJI017 ",
+                    price_regular: "7.500,00",
+                    url: "https://www.google.com.br"
+                },
+                {
+                    id: 3,
+                    title: "Mini drone DJI Mavic Mini 2 DRDJI017",
+                    photo: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/HM9Q2?wid=572&hei=572&fmt=jpeg&qlt=95&.v=1534191258296",
+                    description: "Single com câmera 4K light gray DRDJI017 ",
+                    price_regular: "7.500,00",
+                    url: "https://www.google.com.br"
+                },
+                {
+                    id: 4,
+                    title: "Mini drone DJI Mavic Mini 2 DRDJI017",
+                    photo: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/HM9Q2?wid=572&hei=572&fmt=jpeg&qlt=95&.v=1534191258296",
+                    description: "Single com câmera 4K light gray DRDJI017 ",
+                    price_regular: "7.500,00",
+                    url: "https://www.google.com.br"
+                },
+                {
+                    id: 5,
+                    title: "Mini drone DJI Mavic Mini 2 DRDJI017",
+                    photo: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/HM9Q2?wid=572&hei=572&fmt=jpeg&qlt=95&.v=1534191258296",
+                    description: "Single com câmera 4K light gray DRDJI017 ",
+                    price_regular: "7.500,00",
+                    url: "https://www.google.com.br"
+                },
+                {
+                    id: 6,
+                    title: "Mini drone DJI Mavic Mini 2 DRDJI017",
+                    photo: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/HM9Q2?wid=572&hei=572&fmt=jpeg&qlt=95&.v=1534191258296",
+                    description: "Single com câmera 4K light gray DRDJI017 ",
+                    price_regular: "7.500,00",
+                    url: "https://www.google.com.br"
+                },
+            ],
+            banners_produtos_body: [
+                {
+                    id: 1,
+                    title: "Apple Watch Series 7",
+                    photo: "assets/images/apple-watch.png",
+                    discount: "17% OFF",
+                    description: "Caixa em rosé azul de 45 mm ",
+                    url: "https://www.google.com.br",
+                },
+                {
+                    id: 2,
+                    title: "Apple Watch Series 7",
+                    photo: "assets/images/apple-watch.png",
+                    discount: "17% OFF",
+                    description: "Caixa em rosé azul de 45 mm ",
+                    url: "https://www.google.com.br",
+                },
+            ],
 			settings_produtos_destaque: {
 				itemsToShow: 3.1,
 				snapAlign: "start",
                 wrapAround: true
 			},
-			// breakpoints are mobile first
-			// any settings not specified will fallback to the carousel settings
-			breakpoints: {
+			breakpoints_produtos_destaque: {
 				// 700px and up
 				700: {
 					itemsToShow: 3.5,
@@ -50,7 +231,22 @@ const App = {
 				}
 			}
 		};
-	}
+	},
+    mounted() {
+        // this.$refs.child will hold an instance of <Child />
+        console.log(this.$refs);
+    },
+    methods: {
+        next_produto_destaque() {
+            this.$refs.produtos_detaque.next();
+        },
+        next_banner_todos_os_produtos() {
+            this.$refs.produtos_detaque.next();
+        },
+        prev_produto_destaque() {
+            this.$refs.produtos_detaque.prev();
+        },
+    }
 };
 
 Vue.createApp(App).mount("#app");
--- index.php
+++ index.php
@@ -43,7 +43,6 @@
   <!-- vuejs -->
   <script src="assets/vendor//vuejs/vue.global.js"></script>
   <script src="assets/vendor/vuejs/carousel.js"></script>
-  <!-- "@chenfengyuan/vue-carousel": "http://localhost/tech-vendas/assets/vendor/vuejs/vue-carousel.esm.min.js", -->
 
   <title>Tech Vendas</title>
 </head>
@@ -113,10 +112,10 @@
     <section class="bgBanner">
       <div class="container">
         <div class="blc_banner">
-          <Carousel :autoplay="7000" :wrap-around="true">
-            <Slide v-for="banner in banners" :key="slide">
+          <Carousel :touch-drag='false' :mouse-drag='false' :autoplay="7000" :wrap-around="true">
+            <Slide v-for="banner in banners" :key="banner.id">
               <div class="item">
-                <div class="row d-flex align-items-center ">
+                <div class="row w-100 d-flex align-items-center justify-content-between">
                     <div class="col-12 col-lg-6">
                         <div class="dv1">
                             <span v-if="banner.discount" class="dv2">
@@ -129,17 +128,17 @@
                               {{banner.description}}
                             </p>
                             <div class="blc_preco d-flex">
-                                <del v-if="banner.price_regular" class="preco_regular d-flex align-items-center">
+                                <del v-if="banner.price_regular && banner.price_discount" class="preco_regular d-flex align-items-center">
                                     <span>R$</span> {{banner.price_regular}}
                                 </del>
                                 <ins v-if="banner.price_discount" class="preco_promocao d-flex align-items-start">
                                     <span>R$</span> {{banner.price_discount}}
                                 </ins>
                                 <ins v-if="!banner.price_discount" class="preco_promocao d-flex align-items-start">
-                                    <span>R$</span> {{banner.price_discount}}
+                                    <span>R$</span> {{banner.price_regular}}
                                 </ins>
                             </div>
-                            <a :target="banner.target" v-if="banner.url" :href="banner.url" class="btn d-flex align-items-center justify-content-between btn-primary bt_banner_Saiba_mais">
+                            <a :target="banner.target" v-if="banner.url" :href="banner.url" class="btn d-flex align-items-center justify-content-between btn bt_banner_Saiba_mais">
                                 VER MAIS DETALHES <i class="fal fa-long-arrow-right"></i>
                             </a>
                         </div>
@@ -165,25 +164,208 @@
               Produtos em destaque
             </h2>
             <div class="dv2">
-              <a href="#">
+              <a href="javascript:;" v-on:click="prev_produto_destaque()">
                 <img src="assets/images/left-carrousel.svg" alt="" />
               </a>
-              <a class="dv3" href="#">
+              <a class="dv3" v-on:click="next_produto_destaque()" href="javascript:;">
                 <img src="assets/images/right-carrousel.svg" alt="" />
               </a>
             </div>
             <div class="dv4">
-              <a href="" class="btn d-flex align-items-center justify-content-between btn-primary bt_green_1">
+              <a href="produtos.php" class="btn d-flex align-items-center justify-content-between btn bt_green_1">
                 VER TODOS <i class="fal fa-long-arrow-right"></i>
               </a>
             </div>
           </div>
-          <div class="col-12 col-lg-9">
-            <carousel ref="produtos_detaque" :settings='settings_produtos_destaque'>
-              <slide v-for="slide in 10" :key="slide">
-                <div class="carousel__item">{{ slide }}</div>
+          <div class="col-12 col-lg-9 blc-carrousel-destaque">
+            <carousel :touch-drag='false' :mouse-drag='false' ref="produtos_detaque" :settings='settings_produtos_destaque'>
+              <slide v-for="produto in produtos_destaque" :key="produto.id">
+                <div class="component_produto">
+                  <div>
+                    <a class="img" :href="produto.url" :style="{ background: 'url('+produto.photo+'), #F8F8F8' }"></a>
+                  </div>
+                  <div class="dc0">
+                    <h2 class="dc11">
+                      {{produto.title}}
+                    </h2>
+                    <p class="dc22">
+                      {{produto.description}}
+                    </p>
+                    <div class="blc_preco d-flex justify-content-between">
+                      <del v-if="produto.price_regular && produto.price_discount" class="preco_regular d-flex align-items-center">
+                          <span>R$</span> {{produto.price_regular}}
+                      </del>
+                      <ins v-if="produto.price_discount" class="preco_promocao d-flex align-items-start">
+                          <span>R$</span> {{produto.price_discount}}
+                      </ins>
+                      <ins v-if="!produto.price_discount" class="preco_promocao d-flex align-items-start">
+                          <span>R$</span> {{produto.price_regular}}
+                      </ins>
+                    </div>
+                    <div class="dc23">
+                      <a :href="produto.url" class="btn d-flex align-items-center justify-content-between btn bt_green_1">
+                        SAIBA MAIS <i class="fal fa-long-arrow-right"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
               </slide>
             </carousel>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="todos_os_produtos">
+      <div class="dv1">
+        <div class="container">
+          <div class="row">
+            <div class="col-12 col-lg-3 menuLateral">
+              <div class="input-group search">
+                <input type="text" class="form-control" placeholder="Procure um produto..." aria-label="Procure um produto..." aria-describedby="button-addon2">
+                <button class="btn btn-outline-secondary" type="button" id="button-addon2"><i class="fas fa-search"></i></button>
+              </div>
+              <div class="dv2">
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item d-flex filter-categoria justify-content-between align-items-center">
+                  Categorias
+                </li>
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                  <a title="" class="d-flex active align-items-center w-100 justify-content-between" href="produtos.php">
+                    Todos os produtos <i class="fal fa-angle-right"></i>
+                  </a>
+                </li>
+                <li v-for="categoria in filter_categorias" :key="categoria.id" class="list-group-item d-flex justify-content-between align-items-center">
+                  <a title="" class="d-flex  align-items-center w-100 justify-content-between" :href="categoria.url">
+                    {{categoria.title}} <i class="fal fa-angle-right"></i>
+                  </a>
+                </li>
+                <li class="list-group-item filter-regiao d-flex justify-content-between align-items-center">
+                  REGIÃO
+                </li>
+                <li>
+                  <select class="form-select filter-select form-select-lg">
+                    <option selected>Selecione sua regão</option>
+                    <option v-for="option in filter_regiao" v-bind:value="option.id">{{option.title}}</option>
+                  </select>
+                </li>
+                <li class="list-group-item filter-faixa-preco d-flex justify-content-between align-items-center">
+                  FAIXA DE PREÇO
+                </li>
+                <li>
+                  <ul class="list-unstyled filter-faixa-preco-ul">
+                    <li v-for="price in filter_price" :key="price.id" class="list-group-item no-border d-flex align-items-center">
+                        <input :id="'inputprice'+price.id" name="filter_price" v-bind:value="price.id" class="me-2" type="radio" />
+                        <label :for="'inputprice'+price.id">{{price.title}}</label>               
+                    </li>
+                  </ul>
+                </li>
+                <li class="list-group-item filter-estado-produto d-flex justify-content-between align-items-center">
+                  ESTADO DO PRODUTO
+                </li>
+                <li>
+                  <select class="form-select filter-select form-select-lg">
+                    <option v-for="option in filter_estado_produto" v-bind:value="option.id">{{option.title}}</option>
+                  </select>
+                </li>
+              </ul>
+              </div>
+            </div>
+            <div class="col-12 col-lg-9">
+              <div class="bannertop">
+                <carousel class="bannertop3" :touch-drag='false' :mouse-drag='false' ref="banners_todos_os_produtos">
+                  <slide v-for="banner in banners_produtos_body" :key="banner.id">
+                    <div class="bannertop2 w-100 h-100">
+                      <div class="row">
+                        <div class="col-12 col-lg-6 text-start">
+                          <div>
+                            <span class="bannertop4">{{banner.discount}}</span>
+                          </div>
+                          <h2 class="bannertop5">
+                            {{banner.title}}
+                          </h2>
+                          <p class="bannertop6">
+                            {{banner.description}}
+                          </p>
+                          <div class="bannertop7">
+                            <a :href="banner.url" class="btn d-flex align-items-center justify-content-between btn bt_green_3">
+                              VER MAIS DETALHES <i class="fal fa-long-arrow-right"></i>
+                            </a>
+                          </div>
+                        </div>
+                        <div class="col-12 col-lg-6">
+                          <div class="bannertop_img">
+                            <img alt="" :src="banner.photo"/>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </slide>
+                  <template #addons>
+                    <Navigation />
+                  </template>
+                </carousel>
+              </div>
+              <div class="row">
+                <div v-for="produto in produtos_home" :key="produto.id" class="col-12 col-lg-4">
+                  <div class="component_produto">
+                    <div>
+                      <a class="img" :href="produto.url" :style="{ background: 'url('+produto.photo+'), #F8F8F8' }"></a>
+                    </div>
+                    <div class="dc0">
+                      <h2 class="dc11">
+                        {{produto.title}}
+                      </h2>
+                      <p class="dc22">
+                        {{produto.description}}
+                      </p>
+                      <div class="blc_preco d-flex justify-content-between">
+                        <del v-if="produto.price_regular && produto.price_discount" class="preco_regular d-flex align-items-center">
+                            <span>R$</span> {{produto.price_regular}}
+                        </del>
+                        <ins v-if="produto.price_discount" class="preco_promocao d-flex align-items-start">
+                            <span>R$</span> {{produto.price_discount}}
+                        </ins>
+                        <ins v-if="!produto.price_discount" class="preco_promocao d-flex align-items-start">
+                            <span>R$</span> {{produto.price_regular}}
+                        </ins>
+                      </div>
+                      <div class="dc23">
+                        <a :href="produto.url" class="btn d-flex align-items-center justify-content-between btn bt_green_1">
+                          SAIBA MAIS <i class="fal fa-long-arrow-right"></i>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="dv44 d-flex justify-content-center">
+                <a href="produtos.php" class="btn d-flex align-items-center justify-content-between btn bt_green_2">
+                  VER TODOS OS PRODUTOS <i class="fal fa-long-arrow-right"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="sobre-nos">
+      <div class="container">
+        <div class="row d-flex align-items-center">
+          <div class="col-12 col-lg-6">
+            <div class="dv1">
+              <a class="d-flex justify-content-center align-items-center" href="">
+                <img alt="" src="assets/images/play-icon.svg"/>
+              </a>
+            </div>
+          </div>
+          <div class="col-12 offset-lg-1 col-lg-5">
+            <h2>
+              Um pouco sobre nós
+            </h2>
+            <p>
+              Consequat rhoncus ultrices elit vivamus malesuada porttitor consequat lorem gravida. Eget pellentesque phasellus et habitant. Id pretium dui commodo at aliquet donec ultrices et donec. Viverra rhoncus eu nisi, id arcu ut justo. Duis eu senectus fermentum tempor. At auctor facilisis ut felis aliquet.<br/><br/>
+              Quis dictum laoreet amet arcu aliquet egestas duis massa. Nibh mauris massa duis in ipsum velit, nullam enim. Dictumst viverra fermentum vitae quisque tellus fringilla magna.
+            </p>
           </div>
         </div>
       </div>
@@ -191,93 +373,9 @@
 
     <footer>
       <div class="container">
-        <div class="row">
-            <div class="col-12 text-center text-lg-start col-lg-3">
-              <a title="" href="<?php echo $baseUrl; ?>">
-                <img
-                  src="<?php echo $baseUrl; ?>2022/assets/logo.svg"
-                  alt="Uni"
-                  loading="lazy"
-                  style="margin-top: -1px;"
-                />
-              </a>
-            </div>
-            <div class="col-12 col-lg-2">
-              <ul class="list-unstyled">
-                <li>
-                  Serviços
-                </li>
-                <li>
-                  <a href="<?php echo $baseUrl; ?>sistema/paciente/public/login">
-                    Paciente
-                  </a>
-                </li>
-                <li>
-                  <a href="<?php echo $baseUrl; ?>sistema/doutor/public/login">
-                    Doutor
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div class="col-12 col-lg-3">
-              <ul class="list-unstyled">
-                <li>
-                  Contato
-                </li>
-                <li>
-                  <a href="mailto:contato@appunisaude.com.br">
-                    contato@appunisaude.com.br
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div class="col-12 col-lg-2">
-              <ul class="list-unstyled">
-                <li>
-                  Sobre
-                </li>
-                <li>
-                  <a href="javascript:;" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                    Privacidade
-                  </a>
-                </li>
-              </ul>
-            </div>
-        
-            <div class="col-12 d-flex justify-content-center col-lg-2">
-              <div class="redes">
-                <a class="d-inline-flex justify-content-center align-items-center" target="_blank" href="https://www.instagram.com/unidoctors.oficial/">
-                  <i class="fab fa-instagram"></i>
-                </a>
-                <a class="d-inline-flex justify-content-center align-items-center" target="_blank" href="https://www.facebook.com/Unidoctors-108208857323883">
-                  <i class="fab fa-facebook-f"></i>
-                </a>
-              </div>
-            </div>
-        </div>
-      <div class="col-12 text-center text-lg-start col-lg-3">
-              <a title="" href="https://apps.apple.com/br/app/uni-sa%C3%BAde/id1503661320" target="_others">
-                <img
-                  src="2022/assets/apple.jpg"
-                  alt=""
-                  loading="lazy"
-                  style="margin-top: -1px;"
-                />
-              </a>
-        <a title="" href="https://play.google.com/store/apps/details?id=br.com.unidoctors" target="_others">
-                <img
-                  src="2022/assets/google.jpg"
-                  alt=""
-                  loading="lazy"
-                  style="margin-top: -1px;"
-                />
-              </a>
-            </div>
-        <div class="d-lg-flex dv123 text-center text-lg-start justify-content-between align-items-center">
-          <div>
-            UniDoctors ® 2022 | All rights reserved
-          </div>
-        </div>
+        <p>
+          © Copyright TechVendas - 2022 - Todos os Direitos Reservados
+        </p>
       </div>
     </footer>
     <!-- Modal -->
